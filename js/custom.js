57468f2077ba26310d69d213112aa11b965d60a2
Ana <castlore@gmail.com>
2018-12-12 13:27:03 +0000

    proyecto final

---
+++ js/custom.js
@@ -0,0 +1,4 @@
+// Set the initial heights of the three main DIV's based on user window.
+// Returns height of browser view port
+var window = $(window).height();
+$("header").css('min-height',window);	
